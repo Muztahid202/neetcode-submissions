33ea34adc9bbabdbab03151837d4746a8414e7e8
Masnoon Muztahid <137649037+Muztahid202@users.noreply.github.com>
2026-06-26 12:18:30 +0600

    Add: longest-consecutive-sequence - submission-7

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-7.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        vector<int> copyNums;
+        for(const int &num: nums){
+            copyNums.push_back(num);
+        }
+        //sort the array
+        sort(copyNums.begin(), copyNums.end());
+        int count = 1;
+        int consLen = 1;
+
+        for(int i = 1; i < copyNums.size(); i++)
+        {
+            if(copyNums[i]-copyNums[i-1] == 1) count++;
+            if(count > consLen) consLen = count;
+            if(copyNums[i] - copyNums[i-1] > 1) count = 1;
+        }
+        return consLen;
+
+    }
+};
