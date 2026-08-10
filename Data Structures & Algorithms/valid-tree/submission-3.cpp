db57d14e8d5169d3c96778962a451f4a304d843b
Masnoon Muztahid <137649037+Muztahid202@users.noreply.github.com>
2026-08-10 22:28:37 +0600

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool dfs(int node, int parent, vector<vector<int>>& adj, unordered_set<int>& vis){
+        vis.insert(node);
+
+        for(auto neighbor : adj[node]){
+            if(vis.count(neighbor)){
+                if(neighbor != parent) return true;
+            }
+            else if (dfs(neighbor, node, adj, vis)) return true;
+        }
+        return false;
+    }
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if(edges.size() != n-1) return false;
+
+        //visited array
+        unordered_set<int> vis;
+
+        //construct the graph
+        vector<vector<int>> adj(n);
+        for(const auto& it : edges){
+            int i = it[0];
+            int j = it[1];
+            adj[i].push_back(j);
+            adj[j].push_back(i);
+        }
+
+        //detect if there is cycle
+        if(dfs(0, -1, adj, vis)) return false;
+
+        //if connected or not
+        if(vis.size() != n) return false;
+        
+        return true;
+
+    }
+};
+
+//no cycle + n-1 edges
